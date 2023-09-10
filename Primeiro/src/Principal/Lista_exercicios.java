f593bd96c9b4b2db7c359c78ad5dacbebdbbf51f
Gabriel <Gabriel@DESKTOP-H3F4PU1>
2023-09-10 14:58:56 -0300

    Primeiro Exercicio

---
+++ Primeiro/src/Principal/Lista_exercicios.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Principal;
+
+
+
+import Validacao.Validacao;
+import saida.saida;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Lista_exercicios {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        saida x = new saida();
+        
+        x.Exibir();
+        
+
+    }
+
+}
